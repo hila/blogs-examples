d3877d5b2afd98b4ee26dea5629c9a85c99da0fc
Hila.Kraisler <hila.kraisler@cyberark.com>
2022-05-06 13:34:35 +0300

    First example

---
+++ call-stack-first-example.js
@@ -0,0 +1,11 @@
+function defense() {
+    console.log('I am the defense attorney');
+}
+
+function prosecution() {
+    console.log('I am the prosecutor');
+}
+
+console.log('All rise');
+defense();
+prosecution();
